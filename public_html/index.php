ddc136b4d532c17f03233bb685fd075fc484f308
crawford <freddy_crawford@yahoo.com>
2018-04-19 12:45:56 -0600

    adding content for the girls page..added 2 new photos and change bios for all three girls ..killed parallax effects

---
+++ public_html/index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+
+		<!--BOOTSTRAP CSS-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
+
+		<!--Other and custom CSS-->
+		<link rel="stylesheet" href="styles/stylesheet.css"/>
+
+		<!--link to Bootstrap javascript dependencies-->
+		<!--(Put it here, but if it is a heavy site, put this block just below the closing body tag)-->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+
+		<!--Font Awesome CDN-->
+		<script src="https://use.fontawesome.com/02efcb04d1.js"></script>
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+
+
+		<title>CrawfordGirlsBasketball</title>
+
+	</head>
+
+
+	<body>
+		<!-- nav bar including links for social media and links for page advancement-->
+		<nav class="navbar fixed-top navbar-default navbar-expand-md navbar-dark bg-dark" id="top-of-page">
+			<a class="navbar-brand" href="#">CrawfordGirlsBasketball</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#cailee" >Cailee</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#brooklyn">brooklyn</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Abilities</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Resume</a>
+					</li>
+					<li class="nav-item">
+						<a href="" target="_blank" class="nav-link"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+					</li>
+					<li class="nav-item">
+						<a href="" target="_blank" class="nav-link"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
+					</li>
+					<li class="nav-item">
+						<a href="https://twitter.com/cailee_jordan?lang=en" target="_blank" class="nav-link"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#contact"><i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i></a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+		<div id="carousel1" class="carousel slide" data-ride="carousel">
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img class="d-block w-100" src="images/cailee.jpg" alt="First slide">
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" src="images/caileej.jpg" alt="Second slide">
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" src="..." alt="Third slide">
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+
+
+
+
+		<!--
+      Cailees intro -->
+
+		<div class='' href='cailee'>
+			<h1>Cailee Jordan Crawford</h1>
+			<h3>6 foot / point gaurd / class of 2021</h3>
+			<img src="images/cailee.jpg" alt="basketball player laying up ball">
+			<p>As a 2017 freshman Cailee led her high school to its first 20 win season in over 15 years. She led the team in all categories but rebounds averaging 18.5 points 6.2 rebounds 4 assists 1.4 steals and 1.1 blocks per game. Some of Cailee's accomplishments in her first year of highschool basketball include..All-District First Team, All-Metro first team, All-State second team. Unfortunately Her high school team lost in the first round of the state tournament this year. She vowed to take her team back to the state tournament next year and leave with a title. Cailee has aspirations of playing professional basketball in the WNBA and overseas. She also has a passion for sports medicine and wants to eventually become a sports doctor. One of Cailee's greatest attributes is her ability to handle the ball for her size. She is a great pass first player that can use her great size to see over smaller defenders. She also prides herself on being a great well rounded player that can play any position and contribute in many ways. She is also a good student with a 3.3 GPA. Cailee plays for the New Mexico Clippers 17u AAU team during the summer and will attend multiple AAU tournaments this summer. Maybe Cailee's greatest attribute is her kind heart. She is a great kid with tons of potential.</p>
+			<h5>You can follow Cailee on Twitter</h5> <a href="https://twitter.com/cailee_jordan?lang=en" target="_blank" class="nav-link"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+			<h5>Watch some of Cailee's highlights</h5>
+			<a href="https://www.hudl.com/video/3/10371462/5ad66fa85eecc803442d3023">Watch Me Work</a>
+
+		</div>
+
+
+		<!--
+      Brooklyns intro -->
+		<div class='' href='brooklyn'>
+			<h1>Brooklyn Nevaeh Crawford</h1>
+			<h3>6 foot 1 / Forward / class of 2019</h3>
+			<p>Brooklyn will be a 4 year letterman after next season. She didnt start playing basketball until middle school but has grown her skillset very quickly since then. She has helped to almost double her teams wins each year she has been in high school. She has also led New Mexico 6A girls in blocks each of her 3 seasons. While a knee injury kept her out 10 games this season for the first time in her short career. She was still able to average 7 pts 6 rbs and almost 5 blocks per game and received honorable mention for All-Metro team. I believe she will be a dominant force next year as a senior and predict a dominant season. Brooklyn has aspirations of being a lawyer and wants to play basketball at the college level. Brooklyn has great length and can run the floor well. She has great footwork and can stretch the floor with her range. This summer Brooklyn will also be playing with the NM Clippers 17u AAU team. Brooklyn has a very high ceiling and gets better every season. I look forward to seeing her play at the next level.</p>
+		</div>
+
+
+
+		<!--
+Deani's intro -->
+		<div class='' href='delani'>
+			<h1>Delani 'Boggs' Crawford</h1>
+			<h3>5 foot 10 / point-center / class of 2024</h3>
+			<p>Delani the baby of the bunch has grown up in the gym. Since she was a toddler she has been attending her sisters games and lviving in the gym. Her family used to call her the referree because when she was little she could make calls before the referee had a chance to blow the whistle. She along with being a great player in her own right is her sisters biggest fan. She never misses the opportunity to attend a game and cheer her sisters on. She is agile and very nimble for her size. She started out playing gaurd position but has transitioned to the post based on her current teams need. Out of all the sisters Delani plays with the most ferver. She is very passionate and very intense on the court. Her love for the game is very quickly evident when watching her play. Delani also has aspirations of playing basketball at the professional level. She has all the attributes to make that dream a reality. SHe will be looking forward to her freshman year in highschool when her and her sister Cailee can play together for the first time. Delani recently helped her middle school team to an undefeated season and will look to carry on a tradition started before her by her sisters of multiple midddle school titles.</p>
+
+		</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<!--
+      Contact Form -->
+		<section>
+			<div class="col-lg-6">
+				<form id="contact-form" action="php/mailer.php" method="post">
+					<div class="form-group">
+						<label for="contactName">Name <span class="text-danger">*</span></label>
+						<input type="text" class="form-control" id="contactName" name="contactName" placeholder="Name">
+					</div>
+					<div class="form-group">
+						<label for="contactEmail">Email address <span class="text-danger">*</span></label>
+						<input type="email" class="form-control" id="contactEmail" name="contactEmail" aria-describedby="emailHelp" placeholder="Email">
+						<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+					</div>
+					<div class="form-group">
+						<label for="contactSubject">Subject </label>
+						<input type="text" class="form-control" id="contactSubject" name="contactSubject" placeholder="Name">
+					</div>
+					<div class="form-group">
+						<label for="contactMessage">Message <span class="text-danger">*</span></label>
+						<textarea class="form-control" rows="5" id="contactMessage" name="contactMessage" placeholder="Message"></textarea>
+					</div>
+					<!-- reCAPTCHA -->
+					<!--	<div class="g-recaptcha" data-sitekey="6LeYuUsUAAAAAKAVMIYnHREMF2vTe6MafRmtJaZM"></div>-->
+					<div>
+						<button class="btn btn-success" type="submit">Send</button></div>
+				</form>
+				<!--empty area for form error/success output-->
+				<div class="row">
+					<div class="col-xs-12">
+						<div id="output-area"></div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
